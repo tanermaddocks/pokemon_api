96cafaf1dbb8f2c1ea109266d180cef10dbdc54a
tanermaddocks <maddockstaner@gmail.com>
2025-02-05 19:33:20 +1000

    Created model for team entity

---
+++ src/models/TeamModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const PokemonSchema = new mongoose.Schema({
+	name: String, 
+	sprite: String,
+	types: {
+		type: [String],
+		validate: [limitArrayLength(2), "Cannot have more than 2 types per Pokemon."]
+	},
+	level: {
+		type: Number,
+		min: 1,
+		max: 100
+	},
+});
+
+
+// Make a schema with data properties
+const TeamSchema = new mongoose.Schema({
+	pokemon: {
+		type: [PokemonSchema],
+		validate: [limitArrayLength(6), "Cannot have more than 6 Pokemon per team."]
+	},
+	trainer: {
+		type: mongoose.Types.ObjectId, 
+		ref: 'User'
+	}
+});
+
+function limitArrayLength(limit){
+	return function (value) {
+		return value.length <= limit;
+	}
+}
+
+
+// Make a model that uses the schema 
+//								Name in DB, schema to use for its validation rules 
+const TeamModel = mongoose.model('Team', TeamSchema);
+
+// Export the model 
+module.exports = {
+	TeamModel
+}
