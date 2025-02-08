a83d7622680a88535116827b51855d4143da8977
tanermaddocks <maddockstaner@gmail.com>
2025-02-08 10:50:58 +1000

    Middleware to display pokedata

---
+++ src/middleware/makeTeam.js
@@ -0,0 +1,28 @@
+const { TeamModel } = require("../models/TeamModel");
+
+async function makeTeam (request, response, next){
+	if (!request.customData?.retrievedPokemon){
+		return next(new Error("No data available to make a team with."));
+	}
+
+	let newTeamData = {
+		pokemon: []
+	}
+
+	request.customData.retrievedPokemon.forEach(pokemon => {
+		newTeamData.pokemon.push({
+			name: pokemon.name,
+			sprite: pokemon.sprites.front_default,
+			types: pokemon.types.map(pokeType => pokeType.type.name),
+            level: Math.floor(Math.random() * 100) + 1,
+		})
+	});
+
+    let newTeam = await TeamModel.create(newTeamData)
+
+	next();
+}
+
+module.exports = {
+	makeTeam
+}
